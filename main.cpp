8012dc5a10bed0395c86d2c1d868ce73a2e1f6b2
silviafpp <silviapacheco724@gmail.com>
2024-09-23 10:31:40 +0000

    updates

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int x = 0;
+
+int soma(){
+    int y = x + 1;
+    return y;
+}
+int main() {
+    cout << soma() << endl;
+    return 0;
+}
---
+++ notes/objetos.cpp
@@ -0,0 +1,23 @@
+O que é um objeto?
+
+Um obejto é definido por uma classe
+
+Classe - Forma
+Objeto - Bolo
+
+Como definir uma classe?
+
+Class Carro{
+    public:
+        int velocidade;
+        int velocidadeMax;
+        string nome;
+        void insere(string x, int y);
+        void mostra();
+};
+
+int main() {
+    Carro carro1;
+
+    
+}
