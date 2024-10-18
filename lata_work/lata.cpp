087181c80afbecc991b65f6a933c007f40d5d124
silviafpp <silviapacheco724@gmail.com>
2024-10-18 11:07:51 +0000

    a

--- lata_work/lata.cpp
+++ lata_work/lata.cpp
@@ -1,60 +1,105 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 struct lata {
-    string material;
-    string capacidade;
-    string dimensao;
-    string peso;
-    string sabor;
-    string reciclavel;
-    string dataDeValidade;
-    string lote;
-    string cor;
-    string infoNutricional [5];
-    string lote;
-    string codBarras;
-    string fornecedor;
-
-    bool status;
+        string marca;
+        string sabor;
+        string validade;
+        string fornecedor;
+        string codBarras;
+        string lote;
+        string dataFabricacao;
+        string materialEmbalgem;
+        string infoNutricional[5];
+        double capacidade;
+        double preco;
+        double peso;
+        bool status;
 };
 
-class Refrigerante {
+
+class Refrigerante{
     private:
         lata refrigerante;
+        double qntSumoDisponivel;
 
     public:
 
-        //Constructor
+        //Construtor
         Refrigerante(lata lata){
             refrigerante = lata;
+            qntSumoDisponivel = lata.capacidade;
+        }
+
+        //Abrir a lata
+        void abrirLata(){
+            if (refrigerante.status == false){
+                cout << "A lata esta fechada " << endl;
+            }else{
+                cout << "Abrindo a lata " << endl;
+                refrigerante.status = true;
+            }
+            
+        }
+
+
+        void beberRefrigerante(double qnt){
+            if(refrigerante.status){
+                cout << "ha" << qntSumoDisponivel << "de refrigerante na lata " << endl;
+                if(qnt <= qntSumoDisponivel){
+                    qntSumoDisponivel -= qnt;
+                    cout << "Voce bebeu" << qnt << "litros de referencia" << endl;
+                    }else{
+                        cout << "Nao ha refrigerante sufeciente " << endl;
+                    }
+                }else{
+                    cout << "Voce precisa abrir a lata antes de beber" << endl;
+            }
         }
 };
 
-int main() {
-    lata refri1 = {"Aluminio","350ml","100","10kg","Ananas","Sim","02-03-2025","1234567","Amarelo",{"Calorias: 100","Sódio: 10","Proteina: 100","Açucares: 10g","Lipidos: 10g"}, "21312","54345","6567578"};
+int main(){
+
+    lata lata1 = {"Coca-Cola", "Cola", "10/10/2021",
+                  "Coca-Cola", "123456789", "123456",
+                  "10/10/2020", "Aluminio",
+                                {"Calorias: 100",
+                                "Carboidratos: 30",
+                                "Proteinas: 0",
+                                "Gorduras: 0",
+                                "Sodio: 10"},
+                    350, 5.00, 0.350, false};
+    
+    Refrigerante refri1(lata1);
 
     while (true){
         int opcao;
-        cout << "1 - Abrir lata" << endl;
-        cout << "2 - Beber Refrigerante" << endl;
-        cout << "3 - Sair" << endl;
-        cout << "Escolha uma opção: ";
+        cout << "1 - Abrir lata " << endl;
+        cout << "2 - Beber refrigerante " << endl;
+        cout << "3 - Sair " << endl;
+        cout << "Escolha uma opcao " << endl;
         cin >> opcao;
 
         if(opcao == 1){
-            refri.abrirLata();
+            refri1.abrirLata();
         }else if(opcao == 2){
             double qnt;
-            cout << "Digite a quantidade de refrigerante que deseja beber: " << endl;
+            cout << "Digite a quantidade de refrigerante que deseja beber" << endl;
             cin >> qnt;
             refri1.beberRefrigerante(qnt);
         }else if(opcao == 3){
             break;
         }else{
-            cout << "Opcao invalida" << endl;
+            cout << "Opcao invalida " << endl;
         }
     }
+
+
+
+
+
+
+    
+    return 0;
 }
-
-
